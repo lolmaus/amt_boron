11208f3287865122693603891a700838ba626c4d
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2012-11-14 18:12:41 +0400

    Equal heights for the title page view

---
+++ javascripts/jquery.equalheights.js
@@ -0,0 +1,46 @@
+/**
+ * Equal Heights Plugin
+ * Equalize the heights of elements. Great for columns or any elements
+ * that need to be the same size (floats, etc).
+ * 
+ * Version 1.0
+ * Updated 12/10/2008
+ *
+ * Copyright (c) 2008 Rob Glazebrook (cssnewbie.com) 
+ *
+ * Usage: $(object).equalHeights([minHeight], [maxHeight]);
+ * 
+ * Example 1: $(".cols").equalHeights(); Sets all columns to the same height.
+ * Example 2: $(".cols").equalHeights(400); Sets all cols to at least 400px tall.
+ * Example 3: $(".cols").equalHeights(100,300); Cols are at least 100 but no more
+ * than 300 pixels tall. Elements with too much content will gain a scrollbar.
+ * 
+ */
+
+(function ($) {
+    col = ".view-services-on-title-page .views-row";
+
+    $.fn.equalHeights = function(minHeight) {
+        tallest = (minHeight) ? minHeight : 0;
+        this.each(function() {
+            if($(this).outerHeight() > tallest) {
+                tallest = $(this).outerHeight();
+            }
+        });
+
+        return this.each(function() {
+            $(this).height( tallest );
+        });
+    }
+
+    function doResize() {
+        $(col).equalHeights();
+    }
+
+    $(document).ready(function() {
+       doResize();
+    });
+    $(col).resize(function() {
+       doResize();
+    });
+})(jQuery);
